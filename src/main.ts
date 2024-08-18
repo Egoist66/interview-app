bba589ee7af5cf6a9fc4e499f82629610a7ea197
Farid Mahmudov <Razormad666@gmail.com>
2024-08-18 16:23:26 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import './assets/main.scss'
+import 'primeicons/primeicons.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/aura-light-blue/theme.css'
+
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+
+app.use(createPinia())
+app.use(router)
+app.use({
+    install(app) {
+        console.log('💫 Welcome to the interview app');
+    }
+})
+
+app.mount('#app')
+
+
