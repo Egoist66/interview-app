f206d62987f2a34cc41fbafdb77af2dad0f0c515
Farid Mahmudov <Razormad666@gmail.com>
2024-08-29 12:38:47 +0300

    update

--- src/pages/StatisticsView.vue
+++ src/pages/StatisticsView.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import { useStatistics } from "@/composables/useStatistics";
 
-const { chartData, chartOptions } = useStatistics();
+const { chartData, chartOptions, interviews } = useStatistics();
 </script>
 
 <template>
   <h1 class="text-2xl mb-5 mt-0">Статистика по собеседованиям</h1>
   <div class="card flex justify-content-center">
     <Chart
+      v-if="interviews.length"
       type="doughnut"
       :data="chartData"
       :options="chartOptions"
       class="w-full md:w-30rem"
     />
+
+    <InlineMessage class="block" v-else>Нет данных</InlineMessage>
   </div>
 </template>
 
